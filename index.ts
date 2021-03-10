727e5ccbb1ac1087fab8758dade1ba41b4f021ee
Matthew Man <matthew08m@gmail.com>
2021-03-10 15:24:39 +0000

    fetching from local server

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
 // import express from "express";
-const express = require("express")
+// const query = require("./test-query.sql");
+const express = require("express");
+const { Client } = require("pg");
+
+async function connectLocal() {
+    const client = new Client({
+        user: 'matthewman',
+        host: 'localhost',
+        database: 'musicbase',
+        password: '',
+        port: '5432',
+    });
+    await client.connect().then(console.log("Connected"));
+    const res = await client.query("SELECT * FROM artists")
+    console.log(res.rows);
+};
+
+
 const app = express();
 
-app.get("/", () => console.log("The connection works - root"))
+app.get("/", () => {
+    console.log("The connection works - root"); 
+    connectLocal();
+});
+
 
 app.listen(4000, () => console.log("App is running on local host 4000"));
 
