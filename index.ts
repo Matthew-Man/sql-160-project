b3fe2c2a0affeae2a1cb15a9b04e1aa7f938ac12
Matthew Man <matthew08m@gmail.com>
2021-03-10 15:43:02 +0000

    completed connection and working json returned

--- index.ts
+++ index.ts
@@ -3,25 +3,33 @@
 const express = require("express");
 const { Client } = require("pg");
 
+require("dotenv").config()
+
 async function connectLocal() {
-    const client = new Client({
-        user: 'matthewman',
-        host: 'localhost',
-        database: 'musicbase',
-        password: '',
-        port: '5432',
+        const client = new Client({
+        user: process.env.LOCAL_USER,
+        host: process.env.LOCAL_HOST,
+        database: process.env.DATABASE,
+        password: process.env.LOCAL_PASSWORD,
+        port: process.env.LOCAL_PORT,
     });
-    await client.connect().then(console.log("Connected"));
-    const res = await client.query("SELECT * FROM artists")
-    console.log(res.rows);
+    await client.connect(err => {
+        if (err) {
+          console.error('connection error', err.stack)
+        } else {
+          console.log('connected')
+    }});
+    const res = await client.query("SELECT * FROM artists");
+    return res.rows;
 };
 
 
 const app = express();
 
-app.get("/", () => {
+app.get("/", async (req, res) => {
     console.log("The connection works - root"); 
-    connectLocal();
+    const result = await connectLocal();
+    res.json(result);
 });
 
 
